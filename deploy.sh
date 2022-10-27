b8283af0c5b23d46bc2fea2024b67de9f94436a7
王金平 <32758131+NameWjp@users.noreply.github.com>
2022-10-27 22:42:19 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:NameWjp/visual-drag-demo.git master:gh-pages
+git push -f git@github.com:NameWjp/visual-drag-demo.git main:gh-pages
 
 cd -
